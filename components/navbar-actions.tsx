9d111a573744c2fc1e101fca65160d5aced0d0b8
Daniel <guosongraiden@gmail.com>
2024-02-19 22:13:07 -0800

    add navbar

---
+++ components/navbar-actions.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { ShoppingBag } from "lucide-react";
+import Button from "@/components/ui/button";
+
+const NavbarActions = () => {
+  return (
+    <div className="ml-auto flex items-center gap-x-4">
+      <Button className="flex items-center rounded-full bg-black px-4 py-2">
+        <ShoppingBag size={20} color="white" />
+      </Button>
+    </div>
+  );
+};
+
+export default NavbarActions;
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,19 +1,26 @@
 import Link from "next/link";
 
+import MainNav from "@/components/main-nav";
 import Container from "@/components/ui/container";
+import NavbarActions from "@/components/navbar-actions";
+import getCategories from "@/actions/get-categories";
 
-const NavBar = () => {
-  return (
+const Navbar = async () => {
+  const categories = await getCategories();
+
+  return ( 
     <div className="border-b">
       <Container>
-        <div className="relative flex px-4 md:px-6 lg:px-8 h-16 items-center">
+        <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
           <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
-            <p className="font-bold text-xl">Store</p>
+            <p className="font-bold text-xl">STORE</p>
           </Link>
+          <MainNav data={categories} />
+          <NavbarActions />
         </div>
       </Container>
     </div>
   );
 };
-
-export default NavBar;
+ 
+export default Navbar;
