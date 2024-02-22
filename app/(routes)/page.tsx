a1ebf426ef0f840a1ca94f542e5d7cab739fbcdd
Daniel <guosongraiden@gmail.com>
2024-02-21 23:03:49 -0800

    setup main page
    add billboard and featured products

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,26 @@
-const HomePage = () => {
-  return <div>Hello Store</div>;
+import Container from "@/components/ui/container";
+import Billboard from "@/components/ui/billboard";
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/Product-list";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard("1632f3cd-5277-4ef3-b271-31b1712c5c20");
+  const products = await getProducts({ isFeatured: true });
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+
+        <div className=" flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
